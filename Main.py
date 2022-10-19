61dc4fb9f93ba393d5c8fd8884b2c86ca7bb03c1
URK21CS2037 <109648094+URK21CS2037@users.noreply.github.com>
2022-10-19 09:50:23 +0530

    Update Main.py

--- Main.py
+++ Main.py
@@ -26,11 +26,11 @@ stack = Stack()
 operations = []
 for specific_operation in input().split(','):
     operations.append(specific_operation.strip())
-input_data = input()
-data = input_data.split(',')
+    input_data = input()
+    data = input_data.split(',')
 for i in range(len(operations)):
   if operations[i] == "push":
     stack.push(int(data[i]))
   elif operations[i] == "pop":
     stack.pop()
-stack.status()
+    stack.status()
