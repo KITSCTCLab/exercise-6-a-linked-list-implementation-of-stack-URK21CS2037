6b57204799f9d38e486fd39dbd73c798307eaf20
URK21CS2037 <109648094+URK21CS2037@users.noreply.github.com>
2022-11-02 15:16:06 +0530

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,36 +1,57 @@
 class Node:
   def __init__(self, data):
-    self.data = data 
+    self.data = data
     self.next = None
+#     self.top=None
+
 class Stack:
   def __init__(self):
-    self.head = None 
+    self.top = None
+    #self.top=None
   def push(self, data) -> None:
-    node = Node(data) 
-    node.next = self.head 
-    self.head = node 
+    # Write your code here
+    new=Node(data)
+    new.data=data
+    new.next=self.top
+    self.top=new
   def pop(self) -> None:
-    if(self.head is not None):
-      self.head = self.head.next 
-  def status(self):
-    ptr = self.head 
-    if(self.head is None):
-      print("None") 
+    # Write your code here
+    if self.top==None:
+      return None
     else:
-      while (ptr):
-        print(ptr.data, end="=>") 
-        ptr = ptr.next 
-      print("None") 
+      #temp=Node(data)
+      temp=self.top
+      self.top=self.top.next
+      temp.next=None
+      return temp
+      
+      
+  def status(self):
+    """
+    It prints all the elements of stack.
+    """
+    # Write your code here  
+    if self.top==None:
+      print("None")
+    else:
+      #ptr=Node(data)
+      ptr=self.top
+      while(ptr!=None):
+        print(ptr.data,end="")
+        print("=>",end="")
+        ptr=ptr.next
+        if(ptr==None):
+          print("None")
 # Do not change the following code
 stack = Stack()
 operations = []
 for specific_operation in input().split(','):
     operations.append(specific_operation.strip())
-    input_data = input()
-    data = input_data.split(',')
+input_data = input()
+data = input_data.split(',')
 for i in range(len(operations)):
   if operations[i] == "push":
     stack.push(int(data[i]))
   elif operations[i] == "pop":
     stack.pop()
-    stack.status()
+stack.status() 
